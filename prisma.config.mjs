db11f12b03e17834220c0996e2f5871cb87b5d51
Naman Matoliya <naman.matoliya50962@gmail.com>
2026-07-10 23:32:16 +0530

    setup

---
+++ prisma.config.mjs
@@ -0,0 +1,14 @@
+// prisma.config.mjs
+
+import 'dotenv/config';
+import { defineConfig, env } from 'prisma/config';
+
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: env('DIRECT_URL'), // direct (non-pooled) connection, used for migrations
+  },
+});
