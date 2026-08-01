69ea5442ede175380c879a76088e9bdb370288a1
Naman Matoliya <naman.matoliya50962@gmail.com>
2026-08-01 21:50:12 +0530

    post page

---
+++ components/CreatePostDialog.js
@@ -0,0 +1,12 @@
+'use client'
+
+import {Button} from '@/components/ui/button'
+
+export default function CreatePostDialog(){
+    return (
+
+        <Button onClick={() => alert('Create Post coming next!')}>
+            + Create Post
+        </Button>
+    )
+}
